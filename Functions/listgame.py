2bb92204c2677ddd8ab6646ab0b6c9ad5b9f8c82
Kay <99036659+UlsterHound24@users.noreply.github.com>
2022-04-11 22:59:14 +0700

    Add files via upload

---
+++ Functions/listgame.py
@@ -0,0 +1,59 @@
+import rCSVkay
+
+userid = "kaykay"
+
+# UNTUK MEMBACA
+datag = []                                 
+csv = open("/Users/kayleenchristopher/F09F12/game.csv","r")
+csv_name = "game"
+raw_lines = csv.readlines()
+csv.close()
+clean = rCSVkay.todata(raw_lines, datag, csv_name)
+header = clean[0]
+
+datakepemilikan = []
+kep = open("/Users/kayleenchristopher/F09F12/kepemilikan.csv","r")
+kep_name = "kepemilikan"
+rawk_lines = kep.readlines()
+kep.close()
+cleans = rCSVkay.todata(rawk_lines, datakepemilikan, kep_name)
+heads = cleans[0]
+
+# UNTUK FORMAT OUTPUT
+
+csv_name1 = "game"
+csv_name2 = "kepemilikan"
+
+gamedata = []
+res = rCSVkay.todata(raw_lines, gamedata, csv_name1)
+headergame = res[0]
+
+kepdata = []
+res2 = rCSVkay.todata(rawk_lines, kepdata, csv_name2)
+headerkepemilikan = res2[0]
+
+betterheader = ["ID Game", "Nama Game", "Kategori (Genre)", "Tahun Rilis", "Harga", "Stok"]
+print()
+
+usergame = []
+i = 0
+valid = False
+
+inputuserid = str(input("Silakan input username Anda:"))
+
+for j in datakepemilikan:
+    if(j[1] == inputuserid):
+        for x in datag:
+            if(x[0] == j[0]):
+                usergame += [[x[0], x[1], x[2], x[3], x[4]]]
+                valid = True
+                break
+            else:
+                valid = False
+if not valid:
+    print("Maaf, kamu belum membeli game. Ketik perintah beli_game untuk beli.")
+else:
+    print("Daftar game : ")
+    wordlength = rCSVkay.findLongest(usergame)
+    rCSVkay.neatList(betterheader, usergame, wordlength)
+# Question : Apakah harus ulang input lagi kalau salah input username?
---
+++ Functions/topupsaldo.py
@@ -0,0 +1,39 @@
+import rCSVkay
+
+datau = []                                 
+use = open("/Users/kayleenchristopher/F09F12/user.csv","r")
+use_name = "user"
+rawu_lines = use.readlines()
+use.close()
+cleanes = rCSVkay.todata(rawu_lines, datau, use_name)
+headers = cleanes[0]
+
+inputuser = str(input("Masukan username: "))
+topupsaldo = int(input("Masukan saldo: "))
+valid = False
+
+for i in datau:
+    if(i[1] == inputuser):
+        if((topupsaldo + i[5]) < 0):
+            print("Masukan tidak valid.")
+        else:
+            i[5] = i[5] + topupsaldo
+            saldoakhir = i[5]
+            name = i[2]
+        valid = True
+        break
+    else:
+        valid = False
+
+if not valid:
+    print("Username", inputuser, "tidak ditemukan.")
+else:
+    if (topupsaldo < 0):
+        print("Top up berhasil. Saldo", name, "berkurang menjadi", saldoakhir,".")
+    else:
+        print("Top up berhasil. Saldo", name, "bertambah menjadi", saldoakhir,".")
+
+
+# To do for F12 : Baca file user.csv, tampung semua data di array, check user yang mana yang mau top up saldo, baca saldo,
+#tambahin saldo ke array, tulis lagi semuanya ke csv
+# Question : Apakah harus ulang input lagi kalau salah input username?
