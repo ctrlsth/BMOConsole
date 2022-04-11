328f592e5bca19211f37ba68641c8aa61d397f33
benyaminjodi <89636750+benyaminjodi@users.noreply.github.com>
2022-04-11 23:01:46 +0700

    Add files via upload

---
+++ kerangajaib.py
@@ -0,0 +1,23 @@
+import time
+
+def kerangajaib():
+    t = time.time()
+    question = input("Apa pertanyaanmu? ")
+    magicshell = int(str(t-int(t))[2:])%7
+
+    if magicshell == 0:
+        print("Iya.")
+    elif magicshell == 1:
+        print("Tidak.")
+    elif magicshell == 2:
+        print("Bisa Jadi.")
+    elif magicshell == 3:
+        print("Mungkin.")
+    elif magicshell == 4:
+        print("Tidak mungkin")
+    elif magicshell == 5:
+        print("Tentu.")
+    else:
+        print("YO NDA TAU")
+
+kerangajaib()
