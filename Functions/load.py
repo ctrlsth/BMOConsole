c400037f72f1f4c6a9c1704aa03781a1e6cbfe50
benyaminjodi <89636750+benyaminjodi@users.noreply.github.com>
2022-04-14 14:46:09 +0700

    Add files via upload

---
+++ Functions/load.py
@@ -0,0 +1,68 @@
+import os ; import argparse ; import rCSV as r ; import time
+user = []
+game = []
+riwayat = []
+kepemilikan = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f","--folder", type=str, help="Input harus menyertakan folder")
+args = parser.parse_args()
+
+def load() :
+    global user 
+    global game
+    global riwayat
+    global kepemilikan
+
+
+    if not os.path.exists(args.folder): 
+        print(f'Folder "{args.folder}" tidak ditemukan')
+    else:
+        with open(args.folder + "\\user.csv", "r") as f: 
+            raw_lines = f.readlines()
+            f.close()
+            csv_name = "user"
+            res = r.todata(raw_lines, user , csv_name)
+            header = res[0]
+        with open(args.folder + "\\game.csv", "r") as f: 
+            raw_lines = f.readlines()
+            f.close()
+            csv_name = "game"
+            res = r.todata(raw_lines, game , csv_name)
+            header = res[0]
+        with open(args.folder + "\\riwayat.csv", "r") as f: 
+            raw_lines = f.readlines()
+            f.close()
+            csv_name = "riwayat"
+            res = r.todata(raw_lines, riwayat , csv_name)
+            header = res[0]
+        with open(args.folder + "\\kepemilikan.csv", "r") as f: 
+            raw_lines = f.readlines()
+            f.close()
+            csv_name = "kepemilikian"
+            res = r.todata(raw_lines, kepemilikan , csv_name)
+            header = res[0]
+
+        time.sleep(1)
+        print("Loading...")
+        time.sleep(2)
+        print('''
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@%**@@@@@@@@@++%@@@@@@@*+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@%:::=@@@@@@@%  *@@@@@@%  -@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@%::::%@@@@@@@%  +#*#@@@@#*%@@@%**#@@@@@@#**%@@@@@%**#@@#**%@@@@@%**#@@@@@@@@@@@@
+        @@@@@@@@@%::::%%++%@@@@%    .  =@@  =@@:     =@@+  .  .*@@-    .     -@@*.  .  +@@@@@@@@@@
+        @@@@@@@@@*::-%*:::=@@@@%  -@@%. =@  =@- :@@*  ** .%@@+  %= :@@=  *@@. **  *@@%. #@@@@@@@@@
+        @@@@@@@@@@##@*:::=@@@@@@. -@@%. +@  =@: =@@%  +* .%@@=  %- -@@*  %@@: +*  +@@#. %@@@@@@@@@
+        @@@@@@@@@@@@*:::=@@@@@@@%:  .  =@@  =@: =@@%  +@+  .  .*@- -@@*  %@@: +@*.  .  +@@@@@@@@@@
+        @@@@@@@@@@@@=::=@@@@@@@@@@%#*#@@@@##%@%#%@@@##%@@@#*#%@@@%#%@@@##@@@%#%@@@%#*#@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+        ''')
+
+load()
+
