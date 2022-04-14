a3d878dc16564d97fd79612bfaf2cc41b8a7f006
auliameydiva <103442880+auliameydiva@users.noreply.github.com>
2022-04-14 11:35:09 +0700

    Add files via upload

---
+++ Functions/ubah_stok.py
@@ -0,0 +1,21 @@
+id = input('Masukkan ID: ')
+jumlah = int(input('Masukkan jumlah: '))
+cekid = False
+
+for i in datagame:
+    if i[0] == id :
+        ubahstok = i[5] + jumlah
+        if ubahstok >= 0 :
+            if ubahstok < i[5]:
+                print(f"Stok game {i[1]} berhasil dikurangi. Stok sekarang: {ubahstok}")
+            elif ubahstok >= i[5]:
+                print(f"Stok game {i[1]} berhasil ditambahkan. Stok sekarang: {ubahstok}")
+            cekid = True
+            break
+        else : 
+            print(f"Stok game {i[1]} gagal dikurangi karena stok kurang. Stok sekarang: {i[5]}")
+            cekid = True
+            break
+                  
+if cekid == False :
+    print("Tidak ada game dengan ID tersebut")
