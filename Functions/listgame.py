1615968ec73a1ccfc258909b6001775b2fed8973
auliameydiva <103442880+auliameydiva@users.noreply.github.com>
2022-04-14 19:26:24 +0700

    Add files via upload

---
+++ Functions/listgame.py
@@ -0,0 +1,56 @@
+skema = input('Skema sorting: ')
+
+y = copy(datagame)
+x = length(datagame)
+if skema == 'tahun-' :
+    for i in range(x-1):
+        for j in range(0,x-i-1):
+            if (y[j][3]) < (y[j+1][3]) :
+                y[j], y[j+1] = y[j+1], y[j]
+    for i in y :
+        print(i)
+elif skema == 'tahun+' :
+    for i in range(x-1):
+        for j in range(0,x-i-1):
+            if (y[j][3]) > (y[j+1][3]) :
+                y[j], y[j+1] = ye[j+1], y[j]
+    for i in y :
+        print(i)
+elif skema == 'harga+' :
+    for i in range(x-1):
+        for j in range(0,x-i-1):
+            if (y[j][4]) > (y[j+1][4]) :
+                y[j], y[j+1] = y[j+1], y[j]
+    for i in y :
+        print(i)
+elif skema == 'harga-' :  
+    for i in range(x-1):
+        for j in range(0,x-i-1):
+            if (y[j][4]) < (y[j+1][4]) :
+                y[j], y[j+1] = y[j+1], y[j]
+    for i in y :
+        print(i)
+elif skema == '':
+    for i in range(x-1):
+        for j in range(0,x-i-1):
+            if (y[j][0][4]) < (y[j+1][0][4]) :
+                y[j], y[j+1] = y[j], y[j+1]
+            elif (y[j][0][4]) > (y[j+1][0][4]):
+                y[j], y[j+1] = y[j+1], y[j]
+            else:
+                if (y[j][0][5]) < (y[j+1][0][5]) :
+                    y[j], y[j+1] = y[j], y[j+1]
+                elif (y[j][0][5]) > (y[j+1][0][5]):
+                    y[j], y[j+1] = y[j+1], y[j]
+                else:
+                    if (y[j][0][6]) < (y[j+1][0][6]) :
+                        y[j], y[j+1] = y[j], y[j+1]
+                    elif (y[j][0][6]) > (y[j+1][0][6]):
+                        y[j], y[j+1] = y[j+1], y[j]
+                    else:
+                        y[j], y[j+1] = y[j], y[j+1]
+    for i in y :
+        print(i)
+else:
+    print("Skema sorting tidak valid")
+        
