204ed4557dc52435470460261753cb00ceea582e
Jestrr <94804580+Jestrr@users.noreply.github.com>
2022-04-16 06:37:16 +0700

    ganti kode F08 ( nambahin riwayat, kepemilikan)

--- Functions/F08.py
+++ Functions/F08.py
@@ -1,25 +1,34 @@
 # F08 - Membeli Game
 
-import rCSV_rev as r
+import rCSV as r
 import f03 as l
 
+# riwayat.csv
+header = []
+data = []
+f = open("C:/Users/Alka/riwayat.csv","r")   
+csv_name = "riwayat"     
+raw_lines = f.readlines()                   
+f.close()                                       
+csvtuple = r.todata(raw_lines, header, data, csv_name)      
+
 # kepemilikan.csv
+header2 = []
 data2 = []
 f2 = open("C:/Users/Alka/kepemilikan.csv","r")   
 csv_name2 = "kepemilikan"     
 raw_lines2 = f2.readlines()                   
 f2.close()                                       
-csvtuple2 = r.todata(raw_lines2, data2, csv_name2)      
-header2 = csvtuple2[0]
+csvtuple2 = r.todata(raw_lines2, header2, data2, csv_name2)     
 
 # game.csv
+header3 = []
 data3 = []
 f3 = open("C:/Users/Alka/game.csv","r")   
 csv_name3 = "game"     
 raw_lines3 = f3.readlines()                   
 f3.close()                                       
-csvtuple3 = r.todata(raw_lines3, data3, csv_name3)      
-header3 = csvtuple3[0]
+csvtuple3 = r.todata(raw_lines3, header3, data3, csv_name3)
 
 # login dulu mungkin? Cara dapet data login kyk gini kah?
 loginValid, loginData = l.login()
@@ -27,38 +36,42 @@ loginValid, loginData = l.login()
 
 # ada if (role == "user") disini mungkin?
 def buy_game() :
-    id = loginData[0]
-    saldo = loginData[5]
     in_gameid = str(input("Masukkan ID Game: "))
+    new_riw = []
+    new_kep = []
+    
 
-    for i in data2 :
-        if (int(id) == int(i[1])) :            
-            if (in_gameid == i[0]) :
-                print("Anda sudah memiliki Game tersebut!")
-                break
-            for j in data3 :
-                # print(in_gameid) buat ngecek doang
-                # print(j[0]) buat ngecek doang
-                if ((in_gameid) == (j[0])) :
-                    # print(j) buat ngecek doang
-                    nama_game = j[1]
-                    harga = j[4]
-                    stok = j[5]
-            # print(harga) buat ngecek doang
-            if (saldo < harga) :
-                print("Saldo anda tidak cukup untuk membeli Game tersebut!")
-                break
-            elif (stok <= 0) :
-                print("Stok Game tersebut sedang habis!")
-                break
-            else :
-                print("Game “",nama_game,"” berhasil dibeli!")  # output di nama_gamenya msh ada spasinya, jadi ga sama persis kyk di spesifikasi
-                saldo = saldo - harga
-                stok -= 1
-                break
-                # belom tambahin game tersebut ke riwayat tahun dibeli 2022 + kepemilikan
-        # kalo misal in_gameid gaada gmn? gaada di spesifikasi
+    for i in data :
+        if (loginData[1] == i[3]) :
+            new_riw += [i]
 
-# tulis kode buat modif sama saving disini juga kah? *confusion*
+    for j in data2 :
+        if (int(loginData[0]) == int(j[1])) :
+            new_kep += [j]
+    for j in new_kep :                    
+        if (in_gameid == j[0]) :
+            print("Anda sudah memiliki Game tersebut!")
+            break
+
+    for k in data3 :
+        if ((in_gameid) == (k[0])) :
+            new_game = k
+    if (loginData[5] < new_game[4]) :
+        print("Saldo anda tidak cukup untuk membeli Game tersebut!")
+    elif (new_game[5] <= 0) :
+        print("Stok Game tersebut sedang habis!")
+    else :
+        loginData[5] -= new_game[4] # ngurangin saldo
+        new_riw += [[in_gameid, loginData[2], new_game[4], loginData[1], 2022]] # nambahin riwayat game
+        new_kep += [[in_gameid, loginData[0]]] # nambahin kepemilikan game
+        new_game[5] -= 1 # ngurangin stok game
+        print("Game “" + new_game[1] + "” berhasil dibeli!")
+    # print(loginData) 
+    # print(new_riw)
+    # print(new_kep)
+    # print(new_game)
+
+# loginData[0-5] itu dari user.csv
+# new_game[0-5] itu dari game.csv
 
 # buy_game() # buat ngecek doang
