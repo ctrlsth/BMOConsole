a54266ee03e1c4bd0d6a991b97a376cace1d586b
Kay <99036659+UlsterHound24@users.noreply.github.com>
2022-04-16 09:27:44 +0700

    Update F12.py

--- Functions/F12.py
+++ Functions/F12.py
@@ -1,36 +1,58 @@
-import rCSVk
+# import DummyLoad as l
 
-datau = []
-headeru = []
-use = open("/Users/kayleenchristopher/F09F12/user.csv","r")
-use_name = "user"
-rawu_lines = use.readlines()
-use.close()
-cleanes = rCSVk.todata(rawu_lines, headeru, datau, use_name)
+# Inisiasi
+# headergame = []
+# datagame = []
+# l.game(headergame, datagame)
 
-inputuser = str(input("Masukan username: "))
-topupsaldo = int(input("Masukan saldo: "))
-valid = False
+# headerhis = []
+# datahis = []
+# l.riwayat(headerhis, datahis)
 
-for i in datau:
-    if(i[1] == inputuser):
-        if((topupsaldo + i[5]) < 0):
-            print("Masukan tidak valid.")
-        else:
-            i[5] = i[5] + topupsaldo
-            saldoakhir = i[5]
-            name = i[2]
-        valid = True
-        break
-    else:
-        valid = False
+# headerkep = []
+# datakep = []
+# l.kepemilikan(headerkep, datakep)
 
-if not valid:
-    print("Username", inputuser, "tidak ditemukan.")
-else:
-    if (topupsaldo < 0):
-        print("Top up berhasil. Saldo", name, "berkurang menjadi", saldoakhir,".")
-    else:
-        print("Top up berhasil. Saldo", name, "bertambah menjadi", saldoakhir,".")
+# headeruser = []
+# datauser = []
+# l.user(headeruser, datauser)
 
-# Question : Apakah harus ulang input lagi kalau salah input username?
+# loginData = [2, "hanhan","Hans", "snah", "user", 20000]
+
+
+# F12 - Topup Saldo
+
+# Prosedur
+def topup(datauser):
+
+    username = input("Masukan username: ")
+    topupsaldo = int(input("Masukan saldo: "))
+    valid = False
+
+    for i in datauser:
+        if(i[1] == username):
+            valid = True
+            if((topupsaldo + i[5]) < 0):
+                print("Masukan tidak valid.")
+                break
+            else:
+                i[5] = i[5] + topupsaldo
+                saldoakhir = i[5]
+                name = i[2]
+
+                if (topupsaldo < 0):
+                    print("Top up berhasil. Saldo", name, "berkurang menjadi", saldoakhir,".")
+                else:
+                    print("Top up berhasil. Saldo", name, "bertambah menjadi", saldoakhir,".")
+                break
+            
+    if not valid:
+        print('Username "' + username + '" tidak ditemukan.')
+
+    return
+
+# Test Case
+# topup(datauser)
+# for i in datauser:
+#     if i[0] == 2:
+#         print(i)
