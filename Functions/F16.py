d88dc76e40b00dce89640f311c98c21f4b9b3f0a
benyaminjodi <89636750+benyaminjodi@users.noreply.github.com>
2022-04-19 18:37:11 +0700

    Update F16.py

--- Functions/F16.py
+++ Functions/F16.py
@@ -1,59 +1,39 @@
-# F16 - Save
+import os; from rCSV import * ; from F15 import *
 
-import os ; import argparse
-from random import gammavariate; import time ; import rCSV as r
+def konversi(array): 
+    result =''
+    for line in array :
+        count = 0
+        for word in line:
+            if count == length(line)-1 :
+                result += word
+            else :
+                result += word + ';'
+            count += 1
+        result += '\n'
+    return result
 
-parser = argparse.ArgumentParser(description = "Argparse")
-parser.add_argument('folder', type = str, help = 'Lokasi penyimpanan', default = '')
-args = parser.parse_args()
 
-user = []
-game = []
-riwayat = []
-kepemilikan = []
-def save():
-    global user
-    global game
-    global riwayat
-    global kepemilikan
-
-    folder = input("Masukkan nama csv folder: ")
+def save(): 
+    global user, game, riwayat, kepemilikan
+    word = ''
+    folder = input("Masukan nama folder penyimpanan: ")
     if not os.path.exists(folder):
-        question = input(f"Directory {folder} tidak ada, perlu buat directory?(Y/N) or (y/n) ".format(folder))
-        if question == "Y" or question == "y":
-            os.makedirs(folder)
-            args.folder = folder
-        elif question=="N" or question == "n":
-            print("Mohon maaf, save gagal.")
-        else:
-            print("Inputan yang diberikan tidak benar.")
-            return
-    print()
-    print("Saving...")
-    time.sleep(1.5)
-    with open(args.folder + "\\user.csv", "w+") as f: 
-        for line in user:
-            for x in [line]:  
-                data=r.join(x,";")    
-                f.write(data)
-                f.write("\n")
-    with open(args.folder + "\\game.csv", "w+") as f: 
-        for line in game:
-            for x in [line]:  
-                data=r.join(x,";")
-                f.write(data)
-                f.write("\n")
-    with open(args.folder + "\\riwayat.csv", "w+") as f: 
-        for line in riwayat:
-            for x in [line]:  
-                data=r.join(x,";")
-                f.write(data)
-                f.write("\n")
-    with open(args.folder + "\\kepemilikan.csv", "w+") as f: 
-        for line in kepemilikan:
-            for x in [line]: 
-                data=r.join(x,";")
-                f.write(data)
-                f.write("\n")
-    print("Data berhasil disimpan.")    
+        os.makedirs(folder)
+    for (root,dirs,files) in os.walk(folder, topdown=False):
+        for f in files :
+            os.remove(f'{folder}\\{f}')
+    with open(folder + "\\user.csv", "w+") as f:
+        f.write(konversi(user) + '\n')
+    with open(folder + "\\game.csv", "w+") as f:
+        f.write(konversi(game) + '\n')
+    with open(folder + "\\riwayat.csv", "w+") as f:
+        f.write(konversi(riwayat) + '\n')
+    with open(folder + "\\kepemilikan.csv", "w+") as f:
+        f.write(konversi(kepemilikan) + '\n')
+    print('Saving...')
+    time.sleep(2)
+    print("Data telah disimpan pada folder {folder}!".format(folder))
+
+
 
