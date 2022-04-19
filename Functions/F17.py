0523eeb2c363654fe691725dd6af2b23cf8d6210
benyaminjodi <89636750+benyaminjodi@users.noreply.github.com>
2022-04-19 08:56:04 +0700

    Update F17.py

--- Functions/F17.py
+++ Functions/F17.py
@@ -1,13 +1,30 @@
 def exit():
-    global program
+    # Keluar dari program
+    # I.S. program sedang berjalan
+    # F.S. program ditutup dan selesai
 
+    # KAMUS LOKAL
+    # save_ans : string
+
+    # global variable
+    global program
+    # Function / Procedure
+    # cekYN(jawaban : string) -> boolean
+    # Mengecek input dari user lalu memberi koreksi, harus 'Y' atau 'N'
+    # I.S. string terdefinisi
+    # F.S. Input harus berlaku untuk Y atau N dalam huruf kecil maupun besar.
+
+    # save()
+    # Menyimpan data ke dalam file di folder yang diinputkan
+    # I.S. user, game, riwayat, kepemilikan terdefinisi
+    # F.S. file user, gadget, consumable, gadget_borrow_history, gadget_return_history, consumable_history, inventory_user tersimpan
     # ALGORITMA
-    if <fungsi login>:
+    if login:
         save_ans = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
         while not cekYN(save_ans):
             save_ans = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
         if save_ans == "Y":
-  #          save()
+            save()
     print()
     # Program berhenti
     program = False
@@ -20,7 +37,6 @@ def cekYN(string):
     # I.S. string terdefinisi
     # F.S. Input harus berlaku untuk Y atau N dalam huruf kecil maupun besar.
 
-    
     # KAMUS LOKAL
     
     # ALGORITMA
