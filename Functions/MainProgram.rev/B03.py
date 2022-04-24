de7d3ad2358884a22a10430b98683940b8cf0b5a
benyaminjodi <89636750+benyaminjodi@users.noreply.github.com>
2022-04-24 18:39:36 +0700

    Update B03.py

--- Functions/MainProgram.rev/B03.py
+++ Functions/MainProgram.rev/B03.py
@@ -1,54 +1,57 @@
-
 def tictactoe():
     print('''
-     :+                                           
-*%                                           
- %%                                          
- *%%                                         
-  %%%        .%                              
-   %%%%%%:   :%             %  .             
-    %%%%%    :%:=%          % %%       :     
-    %%%% %   :%=:%%  .%%%   % :%%    %%%     
-   .%%%%%%=  .%# %%%%%%%    %  %%=  %%%.     
-   :%+ %%%%   %%  %%%%.     %   %% %%%       
-   .%#  %%%   %%  %%%#      %   .%%%%        
-    %%%%%%%%. %% %  +%=     %   :%%%         
-    .%%%%##%%:%%      #     %  %%#.%*        
-     ....::#%%%%:.... .    .% :.    %        
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%- 
-  .:=+########%%%##**++=*-:=%..              
-           *  %%%%  %%%%%  -%                
-   %%    %%%: %%%%%%%%%# % +%     +%%%       
-   @%* %%%%-  %% #%%+    % #%    %%%%%.      
-    %%%%%+    %% %%%%.  %% %%   %%%  .*      
-   .%%%*      %% %%.%%=%%  %%  %%%  *%       
-  #%% %%+     %% %%=-%%%   %%  %% *%%        
- +%    +%     %%  %%%%%%%  %%  %%%%#         
-         %    %%   .   %%@ %%    .           
-   ...::::... %%        =%%%%                
-     .-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*=-.   
-              *%  .-=*%%%%%%%%%%%%%%%%%%%%%%%
-              *%           :%%.     ..:::-:: 
-     %%   .@%%=%           .%%%:             
-     %%::%%%%*=%   +%%%     %%+%=+%%%*       
-     .%%%%%.  -%  %%%%%%    %% .%%%%%-       
-     +%%%:    -% %%%   @.   %%  %%%  *%      
-    %%# %%-   :%%%%    *#   %% %%=%%  +%     
-   #%    -%   :%%%     %%   %% %%  #%  %     
-           :  .%%%    #%.   *%.%%   :% %     
-               #%%*:%%%-    .%*%%    :%%     
-               + %%%%@       %%*%%@%%%%@     
-               =  .          %% @%%%%.  +    
-               :                             
-                                                                              
-    ''')
+                :+                                           
+            *%                                           
+            %%                                          
+            *%%                                         
+            %%%        .%                              
+            %%%%%%:   :%             %  .             
+                %%%%%    :%:=%          % %%       :     
+                %%%% %   :%=:%%  .%%%   % :%%    %%%     
+            .%%%%%%=  .%# %%%%%%%    %  %%=  %%%.     
+            :%+ %%%%   %%  %%%%.     %   %% %%%       
+            .%#  %%%   %%  %%%#      %   .%%%%        
+                %%%%%%%%. %% %  +%=     %   :%%%         
+                .%%%%##%%:%%      #     %  %%#.%*        
+                ....::#%%%%:.... .    .% :.    %        
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%- 
+            .:=+########%%%##**++=*-:=%..              
+                    *  %%%%  %%%%%  -%                
+            %%    %%%: %%%%%%%%%# % +%     +%%%       
+            @%* %%%%-  %% #%%+    % #%    %%%%%.      
+                %%%%%+    %% %%%%.  %% %%   %%%  .*      
+            .%%%*      %% %%.%%=%%  %%  %%%  *%       
+            #%% %%+     %% %%=-%%%   %%  %% *%%        
+            +%    +%     %%  %%%%%%%  %%  %%%%#         
+                    %    %%   .   %%@ %%    .           
+            ...::::... %%        =%%%%                
+                .-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*=-.   
+                        *%  .-=*%%%%%%%%%%%%%%%%%%%%%%%
+                        *%           :%%.     ..:::-:: 
+                %%   .@%%=%           .%%%:             
+                %%::%%%%*=%   +%%%     %%+%=+%%%*       
+                .%%%%%.  -%  %%%%%%    %% .%%%%%-       
+                +%%%:    -% %%%   @.   %%  %%%  *%      
+                %%# %%-   :%%%%    *#   %% %%=%%  +%     
+            #%    -%   :%%%     %%   %% %%  #%  %     
+                    :  .%%%    #%.   *%.%%   :% %     
+                        #%%*:%%%-    .%*%%    :%%     
+                        + %%%%@       %%*%%@%%%%@     
+                        =  .          %% @%%%%.  +    
+                        :                             
+                                                                                        
+                ''')
     board = [["#", "#", "#"],
          ["#", "#", "#"],
          ["#", "#", "#"]]
     count = 0
     print("Tic Tac Toe!!")
-    print('''Player 1 : "O" ; Player 2 : "X" ''')
+    print('''Player 1 : "X" ; Player 2 : "O" ''')
     print("Format pengisian input baris lalu kolom")
+    for i in range(3):
+        for j in range(3):
+            print(board[i][j], end = '')
+        print()
 
     while count <= 9:
         print("Player 1 silahkan memasukkan baris kemudian kolom yang ingin dimainkan : ")
@@ -70,69 +73,19 @@ def tictactoe():
                     horizon1 = int(input())
                     vertikal1 = int(input())
 
-        board[horizon1-1][vertikal1-1] = "O"
+
+        board[horizon1-1][vertikal1-1] = "X"
         count += 1
 
+        print("------------------------------------ ")
         for i in range(3):
             for j in range(3):
-                print(board[i][j], end='')
+                print(board[i][j], end='') 
             print()
 
-
-        if count >= 5:
-            if board[0][0] == "O" and board[0][1] == "O" and board[0][1] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
-                print("Player 2 Menang!!!")
-                break
-            elif board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X":
-                print("Player 1 Menang!!!")
-                break
-            elif board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X":
-                print("Player 1 Menang!!!")
-                break
-            elif board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X":
-                print("Player 1 Menang!!!")
-                break
-            elif board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
-                print("Player 1 Menang!!!")
-                break
-            elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
-                print("Player 1 Menang!!!")
-                break
-            elif board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
-                print("Player 1 Menang!!!")
-                break
-            elif board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
-                print("Player 1 Menang!!!")
-                break
-            elif board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
-                print("Player 1 Menang!!!")
-                break
-        if count == 9:
-            print("------Hasil Seri------")
+        if win(count, board) == True:
             break
 
-        
         print("Player 2 silahkan memasukkan baris kemudian kolom yang ingin dimainkan : ")
         horizon2 = int(input())
         vertikal2 = int(input())
@@ -152,13 +105,75 @@ def tictactoe():
                     horizon2 = int(input())
                     vertikal2 = int(input())
 
-        board[horizon2-1][vertikal2-1] = "X"
+        board[horizon2-1][vertikal2-1] = "O"
         count += 1
-
+        
+        print("------------------------------------ ")
         for i in range(3):
             for j in range(3):
                 print(board[i][j], end='')
             print()
-
+         
+        if win(count, board) == True:
+            break
     return
-tictactoe()
+
+
+def win(count, board):
+    flag = False
+    if count >= 5:
+            if board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+                print("Player 2 menang !!!")
+                flag = True
+            elif board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+            elif board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+            elif board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+            elif board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+            elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+            elif board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+            elif board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+            elif board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+                print("Player 1 menang !!!")
+                flag = True
+    if count == 9:
+        print("----------------Hasil Seri-----------------")
+        flag = True
+    return flag
+
+    
+
